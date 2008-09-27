72cf3b0c468432f29b3fafbe4adda10f0a8e93b8
naoya <naoya@hatena.ne.jp>
2008-09-27 14:08:41 +0900

    update README

--- sample.pl
+++ sample.pl
@@ -5,19 +5,11 @@ use warnings;
 use FindBin::libs;
 use Perl6::Say;
 use Algorithm::RangeCoder;
+use Algorithm::RangeCoder::Util;
 
 use constant UCHAR_MAX => 0x100;
 
-# my $str = shift or die "usage: $0 <string>";
-my $str = <<EOF;
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-aliquip ex ea commodo consequat. Duis aute irure dolor in
-reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-culpa qui officia deserunt mollit anim id est laborum.
-EOF
+my $str = shift or die "usage: $0 <string>";
 
 my @char = unpack('C*', $str);
 
@@ -45,7 +37,8 @@ my $bin = $rc->encode($str);
 say "origin:  ", $str;
 say "decoded: ", $rc->decode( $bin );
 
-my $rate = length($bin) / length($str);
+my $rate = 1 - length($bin) / length($str);
 
 say sprintf "origin: %d bytes", length $str;
 say sprintf "encoded: %d bytes (%.1f%%)", length $bin, $rate * 100;
+say unpack('b*', $bin );
